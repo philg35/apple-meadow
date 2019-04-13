d327eb7333eb8f4341da23203ba31aa7e7ddbe53
Philip Gross <philg@sensorswitch.com>
2019-04-13 11:52:41 -0400

    added xml read

--- nLightDevTool/nLightDevTool/ViewController.swift
+++ nLightDevTool/nLightDevTool/ViewController.swift
@@ -18,8 +18,38 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonPressed(_ sender: Any) {
+        print("button pressed")
+        var contents = ""
+        let url = URL(string: "https://10.0.0.251/ngw/devices.xml")!
+        let request = URLRequest(url: url)
         
+        let sessionDelegate = SessionDelegate()
+        let session = URLSession(configuration: .default, delegate: sessionDelegate, delegateQueue: nil)
+        let task = session.dataTask(with: request){(data, response, error) in
+            
+            contents = (String(data: data!, encoding: String.Encoding.utf8) as String?)!
+            print(contents)
+            DispatchQueue.main.async { // Correct
+                self.xmlLabel.text = contents
+            }
+        }
+        task.resume()
     }
     
 }
 
+class SessionDelegate:NSObject, URLSessionDelegate
+{
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        if(challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust)
+        {
+            print("in session delegate")
+            print(challenge.protectionSpace.host)
+            if(challenge.protectionSpace.host == "10.0.0.251")
+            {
+                let credential = URLCredential(trust: challenge.protectionSpace.serverTrust!)
+                completionHandler(URLSession.AuthChallengeDisposition.useCredential, credential)
+            }
+        }
+    }
+}
