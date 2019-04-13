bca368c46d7c2ef884af4df1d3c530c804f6863c
Philip Gross <philg@sensorswitch.com>
2019-04-13 11:50:20 -0400

    added outlets

--- nLightDevTool/nLightDevTool/ViewController.swift
+++ nLightDevTool/nLightDevTool/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var xmlLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+    }
+    
 }
 
