a121128d3faf25e4cd38b9a6b8bf70eae793b0ee
Philip Gross <philg@sensorswitch.com>
2019-07-16 17:45:39 -0400

    added game1

---
+++ game1/game1/ViewController/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  game1
+//
+//  Created by Philip Gross on 7/11/19.
+//  Copyright © 2019 Philip Gross. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            let scene =  MenuScene(size: view.bounds.size)
+                // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
