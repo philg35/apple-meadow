caa71954ca5513b663da98c1a88ba8c4bb1551f4
Philip Gross <philg@sensorswitch.com>
2019-07-27 15:34:26 -0400

    got camera working

--- jumperGame/jumperGame/GameScene.swift
+++ jumperGame/jumperGame/GameScene.swift
@@ -13,32 +13,16 @@ class GameScene: SKScene {
     
     var entities = [GKEntity]()
     var jumped = false
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var player : SKSpriteNode?
-    private var snow : SKEmitterNode?
+    var lastUpdateTime : TimeInterval = 0
+    var player : SKSpriteNode?
+    var cam : SKCameraNode?
     
     override func sceneDidLoad() {
-
         self.lastUpdateTime = 0
-        
         player = self.childNode(withName: "player") as? SKSpriteNode
-        snow = self.childNode(withName: "snow") as? SKEmitterNode
-        snow?.advanceSimulationTime(20)
+        cam = self.childNode(withName: "cameraSprite") as? SKCameraNode
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-
-    }
     func startMovingPlayerLeft () {
         let moveAction = SKAction.move(by: CGVector(dx: -1000, dy: 0), duration: 2)
         player?.xScale = -0.5
@@ -57,106 +41,35 @@ class GameScene: SKScene {
     }
     
     func jumpPlayer () {
-        if (jumped == false)
-        {
+        if (jumped == false) {
             jumped = true
             player?.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 150))
         }
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
-    {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch:UITouch = touches.first!
         let positionInScene = touch.location(in: self)
-//        let touchedNode = self.atPoint(positionInScene)
-//        print(positionInScene)
-        if (positionInScene.x > 0)
-        {
-            if (positionInScene.y > 0)
-            {
+        let pl = player
+        let deltaX = positionInScene.x - (pl?.position.x)!
+        let deltaY = positionInScene.y - (pl?.position.y)!
+        if (deltaX > frame.maxX / 2) {
+            startMovingPlayerRight()
+            if (deltaY > frame.maxY / 2) {
                 jumpPlayer()
-                if positionInScene.x > 100
-                {
-                    startMovingPlayerRight()
-                }
-            }
-            else
-            {
-                startMovingPlayerRight()
             }
         }
-        else
-        {
-            if (positionInScene.y > 0)
-            {
+        else {
+            startMovingPlayerLeft()
+            if (deltaY > frame.maxY / 2) {
                 jumpPlayer()
-                if positionInScene.x < -100
-                {
-                    startMovingPlayerLeft()
-                }
-            }
-            else
-            {
-                startMovingPlayerLeft()
             }
         }
-//        if let name = touchedNode.name
-//        {
-//            print(name, "started")
-//            switch (name)
-//            {
-//            case "arrowLeft":
-//                startMovingPlayerLeft()
-//                break
-//            case "arrowRight":
-//                startMovingPlayerRight()
-//                break
-//            case "Scene":
-//                jumpPlayer()
-//                break
-//            case "snow":
-//                jumpPlayer()
-//                break
-//            default:
-//                break
-//            }
-//        }
-        
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         endMovingPlayer()
-//        let touch:UITouch = touches.first!
-//        let positionInScene = touch.location(in: self)
-//        let touchedNode = self.atPoint(positionInScene)
-//
-//        if let name = touchedNode.name
-//        {
-//            print(name, "ended")
-////            switch (name)
-////            {
-////            case "arrowLeft":
-////                endMovingPlayer()
-////                break
-////            case "arrowRight":
-////                endMovingPlayer()
-////                break
-////            case "Scene":
-////                break
-////            default:
-////                break
-////            }
-//        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
@@ -177,6 +90,11 @@ class GameScene: SKScene {
         if (player?.physicsBody?.velocity.dy == 0)
         {
             jumped = false
+        }
+        
+        if let camera = cam, let pl = player {
+            camera.position.x = pl.position.x + 100
+            camera.position.y = pl.position.y + 100
         }
         
         self.lastUpdateTime = currentTime
