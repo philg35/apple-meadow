b554058e23a28e058aab22aed37bf885d71a62b6
Philip Gross <philg@sensorswitch.com>
2019-08-19 22:16:54 -0400

    formatted events a bit

--- tableviewSwift2/tableviewSwift/DeviceViewController.swift
+++ tableviewSwift2/tableviewSwift/DeviceViewController.swift
@@ -18,12 +18,57 @@ class DeviceViewController : UIViewController {
     override func viewDidLoad() {
         print("in the new class")
         for event in events {
-            eventsTextView.text += event ?? "" + "\r\n"
             
-            if (event?.contains("relay-state"))! {
-                statesTextView.text += event ?? "" + "\r\n"
+            var dictonary: NSDictionary?
+            let t = event?.components(separatedBy: "=")
+            let m = t![0].components(separatedBy: "/")
+            var timeStamp = ""
+            if let data = t?[1].data(using: String.Encoding.utf8) {
+                
+                do {
+                    dictonary = try JSONSerialization.jsonObject(with: data, options: []) as? [String:AnyObject] as NSDictionary?
+                    
+                    if let myDictionary = dictonary
+                    {
+                        print(myDictionary)
+                        timeStamp = (convertDateFormatter(date: myDictionary.value(forKey: "ts") as! String))
+                        
+                        if let state = myDictionary["relay-state"] {
+                            statesTextView.text += "\(timeStamp) = \(state) \r\n"
+                        }
+//                        if (event?.contains("relay-state"))! {
+//                            statesTextView.text += timeStamp + "=" + myDictionary["relay-state"] + "\r\n"
+//                        }
+                        
+                    }
+                } catch let error as NSError {
+                    print(error)
+                }
             }
+            
+            eventsTextView.text += timeStamp + "=" + m.last! + t![1] + "\r\n"
+            
+            
         }
         
+    }
+    
+    func convertDateFormatter(date: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"//this your string date format
+        dateFormatter.timeZone = NSTimeZone(name: "UTC") as TimeZone!
+        dateFormatter.locale = Locale(identifier: "your_loc_id")
+        let convertedDate = dateFormatter.date(from: date)
+        
+        guard dateFormatter.date(from: date) != nil else {
+            assert(false, "no date from string")
+            return ""
+        }
+        
+        dateFormatter.dateFormat = "yyyy-MM-dd EEEE hh:mm:ss a"///this is what you want to convert format
+        dateFormatter.timeZone = NSTimeZone(name: "UTC") as TimeZone!
+        let timeStamp = dateFormatter.string(from: convertedDate!)
+        
+        return timeStamp
     }
 }
