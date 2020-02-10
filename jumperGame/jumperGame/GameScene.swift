a3bb46172f2a76b7e10428c78dad2d9a6ddbf923
Philip Gross <philg@sensorswitch.com>
2020-02-09 19:13:36 -0500

    added goomba level

--- jumperGame/jumperGame/GameScene.swift
+++ jumperGame/jumperGame/GameScene.swift
@@ -17,6 +17,7 @@ enum PhysicsCategories {
     static let peach: UInt32 = 0x1 << 3     // 8
     static let star: UInt32 = 0x1 << 4      // 16
     static let radial: UInt32 = 0x1 << 5    // 32
+    static let goomba: UInt32 = 0x1 << 6    // 64
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
@@ -32,6 +33,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var tileMap : SKTileMapNode?
     
     var playerr: AVAudioPlayer?
+    
+    var level : Int = 0
+    
+    var gameTimer: Timer!
+    
+    var hitsLabel: SKLabelNode!
+    var hits: Int = 0 {
+        didSet {
+            hitsLabel.text = "Hits: \(hits)"
+        }
+    }
+    var highScoreLabel: SKLabelNode!
+    var highScore: Int = 0 {
+        didSet {
+            highScoreLabel.text = "High Score: \(highScore)"
+        }
+    }
     
     override func sceneDidLoad() {
         
@@ -71,6 +89,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         cam = self.childNode(withName: "cameraSprite") as? SKCameraNode
         self.physicsWorld.contactDelegate = self
+        
+        level = UserDefaults.standard.integer(forKey: "Level")
+        
+        if level == 2 {
+            hitsLabel = SKLabelNode(text: "Hits: 0")
+            hitsLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.left
+            hitsLabel.position = CGPoint(x: 20, y: self.frame.size.height - 60)
+            hitsLabel.fontName = "AmericanTypewriter-Bold"
+            hitsLabel.fontSize = 24
+            hitsLabel.fontColor = UIColor.white
+            hits = 0
+            self.addChild(hitsLabel)
+            
+            highScoreLabel = SKLabelNode(text: "High Score: 0")
+            highScoreLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.left
+            highScoreLabel.position = CGPoint(x: 20, y: self.frame.size.height - 60)
+            highScoreLabel.fontName = "AmericanTypewriter-Bold"
+            highScoreLabel.fontSize = 24
+            highScoreLabel.fontColor = UIColor.white
+            highScore = 0
+            self.addChild(highScoreLabel)
+            
+            gameTimer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(addGoomba), userInfo: nil, repeats: true)
+        }
     }
     
     func startMovingPlayerLeft () {
@@ -159,12 +201,63 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 }
             }
         }
+        else if ((firstBody.categoryBitMask & PhysicsCategories.goomba) != 0 && (secondBody.categoryBitMask & PhysicsCategories.player) != 0) {
+                if contact.contactNormal.dy < 0 {
+                    print("got goomba", contact.contactNormal)
+                    if let goombaTest = firstBody.node as! SKSpriteNode? {
+                        if let playerTest = secondBody.node as! SKSpriteNode? {
+                            playerDidCollideWithGoomba(goombaNode: goombaTest, playerNode: playerTest)
+                        }
+                    }
+                }
+        }
         else if ((firstBody.categoryBitMask & PhysicsCategories.ground) != 0 && (secondBody.categoryBitMask & PhysicsCategories.player) != 0) {
             print("landed")
             jumped = false
+            if level == 2 {
+                if hits > highScore {
+                    highScore = hits
+                }
+                hits = 0
+            }
         }
     }
+    
+    
+    @objc func addGoomba() {
+        let goomba = SKSpriteNode(imageNamed: "goomba")
+        goomba.position = CGPoint(x: 1100, y: 320)
+        let size = CGSize(width: 100, height: 100)
+        goomba.scale(to: size)
+        goomba.physicsBody = SKPhysicsBody(rectangleOf: goomba.size)
+        goomba.physicsBody?.isDynamic = true
+        goomba.physicsBody?.allowsRotation = false
+        goomba.physicsBody?.categoryBitMask = PhysicsCategories.goomba
+        goomba.physicsBody?.contactTestBitMask = PhysicsCategories.player
+        goomba.physicsBody?.collisionBitMask = PhysicsCategories.player | PhysicsCategories.ground | PhysicsCategories.goomba
+        self.addChild(goomba)
+        var actionArray = [SKAction]()
+        actionArray.append(SKAction.move(by: CGVector(dx: -5000, dy: 0), duration: 30))
+        actionArray.append(SKAction.removeFromParent())
+        goomba.run(SKAction.sequence(actionArray))
+    }
 
+    func playerDidCollideWithGoomba (goombaNode: SKSpriteNode, playerNode: SKSpriteNode) {
+        let explosion = SKEmitterNode(fileNamed: "Explosion")!
+        explosion.position = goombaNode.position
+        self.addChild(explosion)
+        self.run(SKAction.playSoundFileNamed("stomp.wav", waitForCompletion: false))
+        goombaNode.removeFromParent()
+        self.run(SKAction.wait(forDuration: 0.9)) {
+            explosion.removeFromParent()
+        }
+        hits += 1
+        var impulse = Int((playerNode.physicsBody?.velocity.dy)!) - Int(hits) * 30
+        if impulse < -1000 {
+            impulse = -1000
+        }
+        playerNode.physicsBody?.applyImpulse(CGVector(dx: 0, dy: -impulse/3))
+    }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
@@ -185,6 +278,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if let camera = cam, let pl = player {
             camera.position.x = pl.position.x + 100
             camera.position.y = pl.position.y + 100
+            if level == 2 {
+                hitsLabel.position.x = pl.position.x
+                hitsLabel.position.y = pl.position.y + 200
+                highScoreLabel.position.x = pl.position.x
+                highScoreLabel.position.y = pl.position.y + 150
+            }
         }
         
         self.lastUpdateTime = currentTime
