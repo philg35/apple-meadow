a6e9f7b8404a8400ad44acc552fc915fb9a575e2
Philip Gross <philg@sensorswitch.com>
2020-12-29 14:49:32 -0500

    can't find CocoaMQTT in include. stuck.

--- PhoneLight/PhoneLight/Data/phoneLight.swift
+++ PhoneLight/PhoneLight/Data/phoneLight.swift
@@ -12,17 +12,23 @@ struct PhoneLight: Hashable, Codable, Identifiable {
     var deviceName: String
     var productName: String
     fileprivate var imageName: String
+    var hasOcc: Bool
     var occState: Bool
+    var hasOutput: Bool
     var outputState: Bool
     var level: UInt
+    var deviceId: String
     
-    init(id: Int, deviceName: String, productName: String, imageName: String, occState: Bool, outputState: Bool, level: UInt) {
+    init(id: Int, deviceId: String, deviceName: String, productName: String, imageName: String, occState: Bool, outputState: Bool, level: UInt, hasOcc: Bool, hasOutput: Bool) {
         self.id = id
+        self.deviceId = deviceId
         self.deviceName = deviceName
         self.productName = productName
         self.imageName = imageName
         self.occState = occState
         self.outputState = outputState
         self.level = level
+        self.hasOcc = hasOcc
+        self.hasOutput = hasOutput
     }
 }
