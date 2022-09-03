7db4215eae1272a26e3d23c40387c93806c17a0a
Philip Gross <philg@sensorswitch.com>
2022-09-03 19:37:59 -0400

    changed ui

--- AEStest/AEStest/ContentView.swift
+++ AEStest/AEStest/ContentView.swift
@@ -7,30 +7,52 @@
 
 import SwiftUI
 
+struct BlueButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .frame(minWidth: 100)
+            .background(configuration.isPressed ? Color.yellow : Color.blue)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+                        .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+
+    }
+}
+
 struct ContentView: View {
     @StateObject var ipConn = IPConnection()
     
-    
     var body: some View {
-        VStack {
-        Text("AES test")
-            .padding()
+        GeometryReader { geometry in
             
-            
-        Button(action: {
-            ipConn.send(nlightString: "a5000003fa00fb031b107901010038ee")
-        }) {
-            Text("On")
-        }
-        
-        
-        Button(action: {
-            ipConn.send(nlightString: "a5000003fa00fb031b10790102003bee")
-        }) {
-            Text("Off")
-        }
-            
-            
+            VStack {
+                Text("AES test")
+                    .padding()
+                    .foregroundColor(Color.blue)
+                
+                HStack {
+                    
+                    Spacer()
+                    
+                    Button("On") {
+                        ipConn.send(nlightString: "a5000003fa00fb031b107901010038ee")
+                    }
+                    .buttonStyle(BlueButton())
+                    
+                    Spacer()
+                    
+                    Button("Off") {
+                        ipConn.send(nlightString: "a5000003fa00fb031b10790102003bee")
+                    }
+                    .buttonStyle(BlueButton())
+                    
+                    Spacer()
+                    
+                }
+                
+            }
         }
     }
 }
