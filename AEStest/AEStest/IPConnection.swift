f6e4f5dfdcf102a6a5b9b8a2c19269260e00790a
Philip Gross <philg@sensorswitch.com>
2022-09-03 22:33:31 -0400

    removed some printing

--- AEStest/AEStest/IPConnection.swift
+++ AEStest/AEStest/IPConnection.swift
@@ -42,70 +42,66 @@ class IPConnection : NSObject, ObservableObject {
         self.ipaddress = ipaddress
     }
     
-//    override init() {
-//        super.init()
-//    }
-     
+    
     func send(nlightString : String) -> Void {
         let iv = "00000000000000000000000000000001".data(using: .hexadecimal)!
-        print("iv: \(iv as NSData)")
+        //print("iv: \(iv as NSData)")
         
         let nonceStr = "4102030405060708090a0b0c0de00f00"
-              
+        
         let key = "sensorswitch1234".data(using: .utf8)!
-        print("key: \(key as NSData)")
-              
+        //print("key: \(key as NSData)")
+        
         let dataInStr = nonceStr + nlightString
         let dataIn = dataInStr.data(using: .hexadecimal)!
-        print("dataIn: \(dataIn as NSData)")
+        //print("dataIn: \(dataIn as NSData)")
         
         guard let ciphertext = self.crypt(operation: kCCEncrypt, algorithm: kCCAlgorithmAES, options: kCCOptionPKCS7Padding, key: key, initializationVector: iv, dataIn: dataIn) else { return }
-        print("cipher text: \(ciphertext as NSData)")
+        //print("cipher text: \(ciphertext as NSData)")
         
         let headerString = "0001000000" + "20" + "000000010000"
         let headerData = headerString.data(using: .hexadecimal)!
-        print("header=", headerString.count)
+        //print("header=", headerString.count)
         let client = TCPClient(address: self.ipaddress, port: 5551)
         switch client.connect(timeout: 10) {
-          case .success:
+        case .success:
             switch client.send(data: headerData + ciphertext  ) {
-              case .success:
-                print("sent success")
+            case .success:
                 guard let dataReturnCrypt = client.read(2) else {
-                    print("nothing returned??")
+                    //print("nothing returned??")
                     return }
                 
                 print("dataReturnCrypt text: \(dataReturnCrypt)")
-//                    guard let plaintext = self.crypt(operation: kCCDecrypt, algorithm: kCCAlgorithmAES, options: kCCOptionPKCS7Padding, key: key, initializationVector: iv, dataIn: dataReturnCrypt) else { return }
-//
-//                    print("plain text: \(plaintext as NSData)")
+                //                    guard let plaintext = self.crypt(operation: kCCDecrypt, algorithm: kCCAlgorithmAES, options: kCCOptionPKCS7Padding, key: key, initializationVector: iv, dataIn: dataReturnCrypt) else { return }
+                //
+                //                    print("plain text: \(plaintext as NSData)")
                 
                 
-              case .failure(let error):
+            case .failure(let error):
                 print(error)
             }
-          case .failure(let error):
+        case .failure(let error):
             print(error)
         }
     }
     
     func crypt(operation: Int, algorithm: Int, options: Int, key: Data,
-            initializationVector: Data, dataIn: Data) -> Data? {
+               initializationVector: Data, dataIn: Data) -> Data? {
         return key.withUnsafeBytes { keyUnsafeRawBufferPointer in
             return dataIn.withUnsafeBytes { dataInUnsafeRawBufferPointer in
                 return initializationVector.withUnsafeBytes { ivUnsafeRawBufferPointer in
                     // Give the data out some breathing room for PKCS7's padding.
                     let dataOutSize: Int = dataIn.count + kCCBlockSizeAES128*2
                     let dataOut = UnsafeMutableRawPointer.allocate(byteCount: dataOutSize,
-                        alignment: 1)
+                                                                   alignment: 1)
                     defer { dataOut.deallocate() }
                     var dataOutMoved: Int = 0
                     let status = CCCrypt(CCOperation(operation), CCAlgorithm(algorithm),
-                        CCOptions(options),
-                        keyUnsafeRawBufferPointer.baseAddress, key.count,
-                        ivUnsafeRawBufferPointer.baseAddress,
-                        dataInUnsafeRawBufferPointer.baseAddress, dataIn.count,
-                        dataOut, dataOutSize, &dataOutMoved)
+                                         CCOptions(options),
+                                         keyUnsafeRawBufferPointer.baseAddress, key.count,
+                                         ivUnsafeRawBufferPointer.baseAddress,
+                                         dataInUnsafeRawBufferPointer.baseAddress, dataIn.count,
+                                         dataOut, dataOutSize, &dataOutMoved)
                     guard status == kCCSuccess else { return nil }
                     return Data(bytes: dataOut, count: dataOutMoved)
                 }
--- AEStest/AEStest/NlightPacket.swift
+++ AEStest/AEStest/NlightPacket.swift
@@ -12,13 +12,10 @@ class NlightPacket: NSObject {
     
     func CreatePacket(dest: String, src: String, subj: String, payload: String) -> String {
         let size = 13 + (payload.count / 2)
-        print("size", size)
         let packet : String = "a5" + dest + src + String(format:"%02X", size) + subj + payload
         let packetArray = stringToBytes(packet)
-        print("packetArray=", packetArray!)
         var itemsAtEvenIndices = [Int]()
         var itemsAtOddIndices = [Int]()
-
         for (index, item) in packetArray!.enumerated() {
           if index.isMultiple(of: 2) {
               itemsAtEvenIndices.append(Int(item))
@@ -30,8 +27,6 @@ class NlightPacket: NSObject {
         let ck2 = ~itemsAtOddIndices.reduce(0, {$0^$1})
         let chk1 = String(format:"%02X", ck1).suffix(2)
         let chk2 = String(format:"%02X", ck2).suffix(2)
-        print("odd=", itemsAtOddIndices, String(format:"%02X", ck1).suffix(2))
-        print("even=", itemsAtEvenIndices, String(format:"%02X", ck2).suffix(2))
         return packet + chk1 + chk2
     }
     
